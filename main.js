187b6b22afef6f1824b8644f66f3543cb481f451
shohruzbek <shohruzbekmatyakubov@gmail.com>
2026-05-22 15:56:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+//^ 1. notbuk oling
+let pul = 10.000
+
+if (pul === 1.000) {
+console.log(
+    `Protsessor: Celeron, Pentium yoki Core 2 Duo
+    Tezkor xotira (RAM): 2 GB yoki ko'pi bilan 4 GB.
+    Doimiy xotira: 160 GB - 320 GB HDD (juda sekin ishlaydi).
+    Nuqsonlari: Ko'pincha batareyasi umuman zaryad saqlamaydi (faqat tokda ishlaydi).`
+);
+}
+
+    if (pul === 2.000) {
+    console.log(
+    `Protsessori: Intel Core i3 yoki Core i5 (eskiroq 3- yoki 4-avlodlar).
+    Tezkor xotirasi (RAM): 4 GB yoki ba'zilarida 8 GB bo'lishi mumkin.
+    Doimiy xotirasi: 120 GB yoki 240 GB SSD diskli variantini qidirib topishga harakat qiling. SSD o'rnatilgan bo'lsa, noutbuk qotmasdan tez ishlaydi.
+    Holati: Batareyasi biroz charchagan bo'lishi mumkin, lekin zaryadnikka ulab bemalol ishlatsa bo'ladi.`
+    );
+    }
+    // if (pul = ) {
+    //     console.log ("")
+    // }
+
+    // if (pul = ) {
+    //     console.log ("")
+    // }
+
+    // if (pul = ) {
+    //     console.log ("")
+    // }
+
+    // if (pul = ) {
+    //     console.log ("")
+    // }
